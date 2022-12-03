4a8dc7ca4b24af8bd39a57ea50c1fda73934faef
ZIchunXu <zxu72@my.bcit.ca>
2022-12-02 20:48:13 -0800

    Fixed server cant get header auth

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,27 +1,26 @@
 import axios from "axios";
 
-const MODE : string = process.env.NODE_ENV
+const MODE: string = process.env.NODE_ENV
+
+
 axios.create({
     baseURL: MODE === 'development' ? '/api' : "http://127.0.0.1:7001",
-    withCredentials: true,
-    headers: {
-        'X-Requested-With':'XMLHttpRequest',
-        'Authorization':`${localStorage.getItem('token') || null}`,
-        "Content-type": "application/json"
-    }
-  });
-
+});
+axios.defaults.withCredentials = true
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+axios.defaults.headers.common['Authorization'] = `${localStorage.getItem('token') || null}`;
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.interceptors.response.use(res => {
-    if(typeof res.data !== 'object') {
+    if (typeof res.data !== 'object') {
         console.log('service error')
         return Promise.reject(res)
     }
 
-    if(res.data.code !== 200) {
-        if(res.data.msg) {
+    if (res.data.code !== 200) {
+        if (res.data.msg) {
             console.log(res.data.msg)
         }
-        if(res.data.code === 401) {
+        if (res.data.code === 401) {
             window.location.href = '/login'
         }
         return Promise.reject(res.data)
